df94196c3b2e5416991b9a5f881f4ede294723b1
madyf <madyfouert8@gmail.com>
2022-02-15 11:10:02 -0500

    started the main file

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+# from settings import *
+from debug import debug
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
